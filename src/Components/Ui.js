6d45120cf263c2c20c15ff561abd3ade68266a1e
etiotan <etiotan@gmail.com>
2017-03-09 06:10:59 -0500

    css styling 2

--- src/Components/Ui.js
+++ src/Components/Ui.js
@@ -3,28 +3,17 @@ import React from 'react';
 export default class Ui extends React.Component {
 
     render() {
-/**
- * User interface
- */
-
-
+        /**
+         * User interface
+         */
         return (
             <div className="userInterface">
               <ul>
-                <li>Built for Ryerson FIN300</li>
-                <li>Leave input box empty on the one you would like to solve for</li>
-                <li><span>Restrictions: </span>Periods & interest on bond valuations</li>
-                <li>Full documentation on GitHub</li>
+                <li>Built for Ryerson FIN300 by <a href="https://etiotan.github.io/portfolio/app/" target="_blank" title="Portfolio">Etio</a></li>
+                <li>Solve for empty input box</li>
+                <li>PY and CY defaults at 1</li>
+                <li>Full documentation on <a href="https://github.com/etiotan/react-casio-9750gii" target="_blank" title="README.md">GitHub</a></li>
               </ul>
-              {/* github link */}
-              <div className="contact">
-              <a href="https://github.com/etiotan" target="_blank" title="Documentation">
-                <i className="fa fa-github fa-2x" aria-hidden="true"></i>
-              </a>
-              <a href="https://etiotan.github.io/portfolio/app/" target="_blank" title="Contact Me">
-                <i className="fa fa-user-o fa-2x" aria-hidden="true"></i>
-              </a>
-            </div>
             </div>
         );
 
